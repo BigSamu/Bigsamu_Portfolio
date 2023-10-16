0d22e445d9ff2c7309b20b746327800a39133d84
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-10-16 19:44:16 +0100

    updating portfolio profile - 16/10/2023

--- src/data/userDetails.js
+++ src/data/userDetails.js
@@ -1,7 +1,7 @@
 export default {
   fullname: 'Samuel Valdes Gutierrez',
   nickname: 'BigSamu',
-  cv_url: 'https://1drv.ms/b/s!Asber9wcpjf8j4V8Ku7CtswQJm90eA?e=R4yIPv',
- 
-  
+  cv_url: 'https://1drv.ms/b/s!Asber9wcpjf8kZ1sFoVj5UKjMjpsDg?e=vKI54K',
+
+
 };
