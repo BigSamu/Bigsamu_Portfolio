40ee89576fc05e7572fc0533be568a8269b0594c
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2026-02-17 23:00:38 -0300

    Update CV URL in userDetails.js

--- src/data/userDetails.js
+++ src/data/userDetails.js
@@ -1,5 +1,5 @@
 export default {
   fullname: 'Samuel Valdes Gutierrez',
   nickname: 'BigSamu',
-  cv_url: 'https://1drv.ms/b/c/fc37a61cdcafdec6/IQA49g_qVfHYSrcgnGN8wSpLAdDmvnYNCcbNhIM2UWvkjGI?e=Uiljj9',
+  cv_url: 'https://1drv.ms/w/c/fc37a61cdcafdec6/IQB4lFYFu7haTJe-50kEwI05AeQ28ChX2kEZkBopy57Qc9M',
 };
